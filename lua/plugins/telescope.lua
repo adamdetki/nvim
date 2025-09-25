89c980b4cd13782411be0949e97104f2f1ab2fa5
Adam Detki <adam.detki@pm.me>
2025-09-25 08:09:42 +0200

    Update for new version

--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
--- I don't think telescope is in use in the new version of Lazyvim, in which case this is unnecessary.
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
-      file_ignore_patterns = { "%.test%." }, -- Ignore .test files
-    },
-  },
-}
